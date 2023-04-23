7f6d57124084bd1fe536dd6928ce2330a4b426c7
rsevial <rebekahsevial@gmail.com>
2023-04-23 22:16:05 +0800

    Open the text file

---
+++ Assign 3.1 - Seperate even and odd numbers from text file.py
@@ -0,0 +1,12 @@
+# Programmed by: Rebekah Joy E. Sevial
+# Problem 1 of Assignment 3
+# Seperate even and odd numbers from text file
+
+# Open text file
+with open("numbers.txt") as file_numbers:
+# For loop to read each line of the text file
+# If-else statement that determines if the number in the text file is even or odd number
+# Compute for the squared
+# Create new text file for even numbers as even.txt
+# Compute for the cube
+# Create new text file for odd numbers as odd.txt
