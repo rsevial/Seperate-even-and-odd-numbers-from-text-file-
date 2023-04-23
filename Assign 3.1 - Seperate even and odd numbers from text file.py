47d53f51ef6913c6851036c8fb7212b8b971475d
rsevial <rebekahsevial@gmail.com>
2023-04-23 23:26:56 +0800

    If statement for converting string into

--- Assign 3.1 - Seperate even and odd numbers from text file.py
+++ Assign 3.1 - Seperate even and odd numbers from text file.py
@@ -8,8 +8,12 @@ def seperated_number():
     with open("numbers.txt") as file_numbers:
         # For loop to read each line of the text file
         for line in file_numbers:
+            # If statement for converting string into a integer
+            if line.strip:
+                input_numbers = int(line)
     # If-else statement that determines if the number in the text file is even or odd number
-            # If the number is even
-                # Create new text file for even numbers as even.txt
+                # If the number is even
+                    # Create new text file for even numbers as even.txt      
+                # Else
                 # Create new text file for odd numbers as odd.txt
 # Process that will end the program
