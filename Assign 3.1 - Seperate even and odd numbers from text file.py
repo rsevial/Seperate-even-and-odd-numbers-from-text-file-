c89c50bce4d731f6c8c7234e9346bb5ae8161e01
rsevial <rebekahsevial@gmail.com>
2023-04-23 22:54:16 +0800

    Define seperate numbers

--- Assign 3.1 - Seperate even and odd numbers from text file.py
+++ Assign 3.1 - Seperate even and odd numbers from text file.py
@@ -2,12 +2,14 @@
 # Problem 1 of Assignment 3
 # Seperate even and odd numbers from text file
 
+# Define seperated numbers
+def seperated_number():
 # Open text file
-with open("numbers.txt") as file_numbers:
-    # For loop to read each line of the text file
-    for line in file_numbers:
-# If-else statement that determines if the number in the text file is even or odd number
-# Compute for the squared
-# Create new text file for even numbers as even.txt
-# Compute for the cube
-# Create new text file for odd numbers as odd.txt
+    with open("numbers.txt") as file_numbers:
+        # For loop to read each line of the text file
+        for line in file_numbers:
+    # If-else statement that determines if the number in the text file is even or odd number
+            # If the number is even
+                # Create new text file for even numbers as even.txt
+                # Create new text file for odd numbers as odd.txt
+# Process that will end the program
